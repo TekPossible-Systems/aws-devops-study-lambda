1c94bb69de2b87098d8e34b6baf6e2f6f72db4b5
TekPossible-ES <156703579+TekPossible-ES@users.noreply.github.com>
2025-02-23 11:25:23 -0700

    forgot to git pull there- oops

--- server/userdata.sh
+++ server/userdata.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-dnf install -y python3-pip httpd mod_ssl
+dnf install -y python3-pip httpd mod_ssl git
 pip3 install requests boto3 Flask
+cd /root
+git clone git@github.com:TekPossible-Systems/aws-devops-study-lambda.git
+cd aws-devops-study-lambda/server
 cp *.py /root
 cp ssl.conf /etc/httpd/conf.d/
 systemctl restart httpd
