fbc1fa1937f819f39fc5bbe8455d76026ce5d9da
TekPossible-ES <156703579+TekPossible-ES@users.noreply.github.com>
2025-02-23 11:12:21 -0700

    Added the ASG into the code and made it so that the userdata et al is called correctly

--- aws-cluster-cdk/lib/aws-cluster-cdk-stack.ts
+++ aws-cluster-cdk/lib/aws-cluster-cdk-stack.ts
@@ -5,7 +5,11 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigatewayv2';
 import * as apigw_int from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import cluster from 'cluster';
+import { readFileSync } from 'fs';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsClusterCdkStack extends cdk.Stack {
@@ -14,6 +18,7 @@ export class AwsClusterCdkStack extends cdk.Stack {
 
     // Create a VPC
     const cluster_vpc = new ec2.Vpc(this, 'Cluster_VPC', {
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
       subnetConfiguration: [
         {
           cidrMask: 24, 
@@ -75,10 +80,50 @@ export class AwsClusterCdkStack extends cdk.Stack {
 
     ssm_param_hosts.grantRead(lambda_function);
 
+    const iam_role = new iam.Role(this, "ec2-instancerole", {
+      roleName: 'ec2-cluster-instance-role',
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com")
+    });
 
-    // TODO: Add ASG/EC2 instances
-    // ssm_param_hosts.grantRead() EC2 Instances
-    // ssm_param_hosts.grantWrite() EC2 Instances
+    iam_role.addManagedPolicy(iam.ManagedPolicy.fromManagedPolicyArn(this, "Cluster_SSM_Role", "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"));
+
+    const instanceProfile = new iam.InstanceProfile(this, "ec2-instanceprofile", {
+      instanceProfileName: "cluster-ec2-instance-profile",
+      role: iam_role
+    });
+
+    const securityGroup = new ec2.SecurityGroup(this, 'ec2-cluster-sg', {
+      securityGroupName: "Cluster_Security_Group",
+      vpc: cluster_vpc,
+    });
+    securityGroup.addIngressRule(ec2.Peer.ipv4("10.0.0.0/16"), ec2.Port.HTTPS, "Cluster WEBAPI OUT")
+    var userdata_commands = readFileSync("../server/userdata.sh", "utf-8");
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(userdata_commands);
+    // Add ASG/EC2 instances
+    const launchTemplate = new ec2.LaunchTemplate(this, 'EC2-Cluster-LT', {
+      requireImdsv2: true,
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      instanceProfile: instanceProfile,
+      instanceType: new ec2.InstanceType("m4.xlarge"),
+      launchTemplateName: "Cluster_LaunchTemplate",
+      userData: userData,
+      securityGroup: securityGroup
+
+    })
+    const scaling_group = new autoscaling.AutoScalingGroup(this, 'Cluster_ASG', {
+      vpc: cluster_vpc,
+      launchTemplate: launchTemplate,
+      minCapacity: 6,
+      maxCapacity: 8,
+      vpcSubnets: cluster_vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+      }),
+      autoScalingGroupName: "Cluster_ASG"
+    });
+
+    ssm_param_hosts.grantRead(iam_role) 
+    ssm_param_hosts.grantWrite(iam_role)
 
 
     const api_gw_url = new ssm.StringParameter(this, 'ssm-api-gw-url', {
